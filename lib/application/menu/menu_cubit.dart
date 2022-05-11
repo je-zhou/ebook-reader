0dd407aeb1761286c537dcc60d35fe0d68a7399c
JEZHOU99 <jerry.zhou07@gmail.com>
2022-05-11 22:38:13 +1000

    Menu and Topbar now follow menu cubit

--- lib/application/menu/menu_cubit.dart
+++ lib/application/menu/menu_cubit.dart
@@ -7,8 +7,27 @@ part 'menu_cubit.freezed.dart';
 class MenuCubit extends Cubit<MenuState> {
   MenuCubit() : super(MenuState.library());
 
+  intToState(int index) {
+    switch (index) {
+      case 0:
+        navigateToLibrary();
+        break;
+      case 1:
+        navigateToQuotes();
+        break;
+      case 2:
+        navigateToSettings();
+        break;
+      default:
+        throw Error();
+    }
+  }
+
+  int stateToInt() {
+    return state.map(library: (_) => 0, quotes: (_) => 1, settings: (_) => 2);
+  }
+
   navigateToLibrary() => emit(MenuState.library());
   navigateToQuotes() => emit(MenuState.quotes());
   navigateToSettings() => emit(MenuState.settings());
-
 }
--- lib/presentation/menu.dart
+++ lib/presentation/menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:test_drive/application/menu/menu_cubit.dart';
 
 class Menu extends StatelessWidget {
   const Menu({Key? key}) : super(key: key);
@@ -7,10 +9,26 @@ class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const items = [
-      BottomNavigationBarItem(label: "Library", icon: Icon(FontAwesomeIcons.bookOpen)),
-      BottomNavigationBarItem(label: "Quotes", icon: Icon(FontAwesomeIcons.noteSticky)),
-      BottomNavigationBarItem(label: "Settings", icon: Icon(FontAwesomeIcons.gear))
+      BottomNavigationBarItem(
+          label: "Library", icon: Icon(FontAwesomeIcons.bookOpen)),
+      BottomNavigationBarItem(
+          label: "Quotes", icon: Icon(FontAwesomeIcons.noteSticky)),
+      BottomNavigationBarItem(
+          label: "Settings", icon: Icon(FontAwesomeIcons.gear))
     ];
-    return BottomNavigationBar(items: items, key: const Key('bottomNavigationBar'));
+    return BlocBuilder<MenuCubit, MenuState>(
+      builder: (context, state) {
+        MenuCubit cubit = context.read<MenuCubit>();
+
+        return BottomNavigationBar(
+          currentIndex: cubit.stateToInt(),
+          items: items,
+          key: const Key('bottomNavigationBar'),
+          onTap: (int index) {
+            cubit.intToState(index);
+          },
+        );
+      },
+    );
   }
 }
