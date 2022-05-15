d265b3c8766c6d9445713383947113d8cc55dfc1
JEZHOU99 <81694589+Je-Zhou@users.noreply.github.com>
2022-05-15 21:48:26 +1000

    Working Widget Tests with mock cubit implementation
    When creating mock cubits we need to assign functionality to it.
    
    This means we need to specify what the initial state is and what the functions will return if used by the widget we are testing. We can do this with the mocktail's "when()" method.
    
    For our Menu widget:
    - We need to tell our mock cubit what the initial state will be and;
    - We need to specify a value that the stateToInt() method will return because it is used by the widget to determine the current active page.
    
    Resources:
    ResoCoder's Flutter Testing Guide for Beginners
    Part 1 - Unit Tests: https://www.youtube.com/watch?v=hUAUAkIZmX0&list=RDCMUCSIvrn68cUk8CS8MbtBmBkA&index=1
    Part 2 - Widget & Integration Tests: https://www.youtube.com/watch?v=Ghqry5dtgH4&list=RDCMUCSIvrn68cUk8CS8MbtBmBkA&index=2

--- test/menu_widget_test.dart
+++ test/menu_widget_test.dart
@@ -1,20 +1,57 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:test_drive/application/page_selector/page_selector_cubit.dart';
 import 'package:test_drive/presentation/menu.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockPageSelectorCubit extends MockCubit<PageSelectorState>
+    implements PageSelectorCubit {}
 
 void main() {
-  testWidgets('Menu contains library, quotes, settings test',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(
-        const MaterialApp(home: Scaffold(bottomNavigationBar: Menu())));
+  late MockPageSelectorCubit mockCubit;
 
+  setUp(() {
+    mockCubit = MockPageSelectorCubit();
+  });
+
+  void arrangeInitialStateIsLibraryPage() {
+    when(() => mockCubit.state).thenReturn(const PageSelectorState.library());
+  }
+
+  // When the Menu Widget calls cubit.stateToInt() method, our mock cubit will
+  // return 0 so that the library tab will be the active tab
+  void arrangePageSelectorCubitReturnsIndex0ForMenuWidget() {
+    when(() => mockCubit.stateToInt()).thenReturn(0);
+  }
+
+  Widget createWidgetUnderTest() {
+    return BlocProvider<PageSelectorCubit>(
+      create: (context) => mockCubit,
+      child: const MaterialApp(home: Scaffold(bottomNavigationBar: Menu())),
+    );
+  }
+
+  testWidgets('Menu contains library, quotes, settings text and icon',
+      (WidgetTester tester) async {
+    // arrange
+    arrangeInitialStateIsLibraryPage();
+    arrangePageSelectorCubitReturnsIndex0ForMenuWidget();
+
+    // find
     final libraryText = find.text('Library');
     final libraryIcon = find.byIcon(FontAwesomeIcons.bookOpen);
     final quotesText = find.text('Quotes');
     final quotesIcon = find.byIcon(FontAwesomeIcons.noteSticky);
     final settingsText = find.text('Settings');
     final settingsIcon = find.byIcon(FontAwesomeIcons.gear);
+
+    // test
+    await tester.pumpWidget(createWidgetUnderTest());
+
+    // expect
     expect(libraryText, findsOneWidget);
     expect(libraryIcon, findsOneWidget);
     expect(quotesText, findsOneWidget);
