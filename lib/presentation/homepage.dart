d1566b38287f0a3d5db1b95e4cebe2c7c71cc940
JEZHOU99 <jerry.zhou07@gmail.com>
2022-05-19 21:25:35 +1000

    added read file permission

--- lib/presentation/homepage.dart
+++ lib/presentation/homepage.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:test_drive/presentation/pages/page_wrapper.dart';
+import 'package:path_provider/path_provider.dart' as provider;
 
 import 'topbar.dart';
 import 'menu.dart';
@@ -9,6 +12,16 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Directory _photoDir = Directory('/storage/emulated/0/EbookReader');
+
+    readDirectory() {
+      print(_photoDir.existsSync());
+      var list = _photoDir.listSync().map((e) => e).toList();
+      print(list);
+    }
+
+    readDirectory();
+
     return const Scaffold(
       appBar: Topbar(),
       body: PageWrapper(),
--- lib/presentation/topbar.dart
+++ lib/presentation/topbar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:test_drive/application/book_view/book_view_cubit.dart';
-import 'package:test_drive/domain/book/book.dart';
 
 import '../application/page_selector/page_selector_cubit.dart';
 
