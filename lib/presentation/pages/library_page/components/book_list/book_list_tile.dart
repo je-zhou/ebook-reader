f0f0a50d29e6cb099988cc792a84d00ae5ead7d9
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-05-19 21:08:31 +1000

    add book_list tile widget with book name and author

--- lib/presentation/pages/library_page/components/book_list/book_list_tile.dart
+++ lib/presentation/pages/library_page/components/book_list/book_list_tile.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../domain/book/book.dart';
+
 class BookListTile extends StatelessWidget {
-  const BookListTile({Key? key}) : super(key: key);
+  const BookListTile({Key? key, required this.book}) : super(key: key);
+
+  final Book book;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child:  Column(
+          children:[
+            Row(children:[
+              Text(book.title),
+            ]),
+            Row(children:[
+              Text(book.author),
+            ])
+          ])
+    );
   }
 }
