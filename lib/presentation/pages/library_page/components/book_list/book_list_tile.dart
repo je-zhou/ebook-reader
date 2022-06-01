7332c125e6d7d8daa25ca79ab0c1552f8246adec
JEZHOU99 <jerry.zhou07@gmail.com>
2022-06-02 00:04:56 +1000

    style book view list to look to figma design

--- lib/presentation/pages/library_page/components/book_list/book_list_tile.dart
+++ lib/presentation/pages/library_page/components/book_list/book_list_tile.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 import '../../../../../domain/book/book.dart';
@@ -11,20 +9,79 @@ class BookListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // List Tiles will be 1/7 of screen height
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    // TODO:: Read progress percentage from db
+    // Placeholder percentage
+    double placeholderPercentage = 0.1;
+
     return Container(
-        height: 100,
-        child: Row(children: [
-          Image.memory(book.img),
-          Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+      height: screenHeight / 7,
+      margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(4)),
+            child: Image.memory(book.img),
+          ),
+          const SizedBox(width: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(book.title,
-                    style: const TextStyle(
-                        fontSize: 14, fontWeight: FontWeight.bold)),
-                // TODO:: Seperate Multiple Authors by , e.g "Jerry Zhou, Alvin Zhao"
-                Text(book.authors.first, style: const TextStyle(fontSize: 12)),
-              ]),
-        ]));
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      book.title,
+                      style: const TextStyle(
+                          fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      book.authors.join(", "),
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                      width: 20,
+                      child: CircularProgressIndicator(
+                        value: placeholderPercentage,
+                        backgroundColor: Colors.grey,
+                        color: Colors.black54,
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Text('${placeholderPercentage * 100}%')
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      book.fileType.substring(1).toUpperCase(),
+                      style: const TextStyle(
+                          fontSize: 12, fontWeight: FontWeight.w300),
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      '${book.numOfChapters} Chapters',
+                      style: const TextStyle(
+                          fontSize: 12, fontWeight: FontWeight.w300),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
