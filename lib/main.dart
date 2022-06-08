665ce0dc8a0c251ade1d6ad426c57302ea6c6974
JEZHOU99 <jerry.zhou07@gmail.com>
2022-06-08 22:35:39 +1000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,16 @@ import 'package:test_drive/presentation/wrapper.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() {
+  // Initialising database
+  Hive.initFlutter();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // Initialising database
-    Hive.initFlutter();
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
