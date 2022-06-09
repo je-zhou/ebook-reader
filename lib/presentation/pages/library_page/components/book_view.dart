b9ca037cd616a14835188d761d9c086d7ca57b32
JEZHOU99 <jerry.zhou07@gmail.com>
2022-06-09 22:31:43 +1000

    Update book_view.dart

--- lib/presentation/pages/library_page/components/book_view.dart
+++ lib/presentation/pages/library_page/components/book_view.dart
@@ -1,38 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test_drive/application/book_view/book_view_cubit.dart';
-import 'package:test_drive/domain/book/book_repository.dart';
 import 'package:test_drive/presentation/pages/library_page/components/book_grid/book_grid.dart';
 import 'package:test_drive/presentation/pages/library_page/components/book_list/book_list.dart';
-import 'package:test_drive/utils.dart';
 
-import '../../../../domain/book/book.dart';
-
+import '../../../../application/book_loader/book_loader_cubit.dart';
 
 class BookView extends StatelessWidget {
   const BookView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    BookRepository bookRepository = BookRepository(Theme.of(context).platform);
-    return FutureBuilder(
-        future: bookRepository.getBooks(),
-        builder: ((context, snapshot) =>
-            BlocBuilder<BookViewCubit, BookViewState>(
-              builder: (context, state) {
-                if (snapshot.hasData) {
-                  var books = snapshot.data as List<Book>;
-                  if (state.isSearch) {
-                    books = books.filterBookBySearch(state.searchBy);
-                  }
-                  if (state.isGridView) {
-                    return BookGrid(books: books);
-                  } else {
-                    return BookList(books: books);
-                  }
-                }
-                return const CircularProgressIndicator();
-              },
-            )));
+    return BlocBuilder<BookLoaderCubit, BookLoaderState>(
+      builder: ((context, loaderState) {
+        return loaderState.map(
+          initial: (_) => const CircularProgressIndicator.adaptive(),
+          loading: (_) => const CircularProgressIndicator.adaptive(),
+          loadSuccess: (e) => BlocBuilder<BookViewCubit, BookViewState>(
+            builder: (context, viewState) {
+              if (viewState.isGridView) {
+                return BookGrid(books: e.books);
+              } else {
+                return BookList(books: e.books);
+              }
+            },
+          ),
+          loadFailure: (e) => Center(
+            child: Text('Could not load books because: ${e.errorMsg}'),
+          ),
+        );
+      }),
+    );
   }
 }
