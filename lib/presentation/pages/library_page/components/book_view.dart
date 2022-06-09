8f1645142e6665b89ee9ceebfcc26108a9d749d0
JEZHOU99 <jerry.zhou07@gmail.com>
2022-06-09 21:26:47 +1000

    add book loader cubit
    Now we can refresh the cubit when we close the ebook reader widget

--- lib/presentation/pages/library_page/components/book_view.dart
+++ lib/presentation/pages/library_page/components/book_view.dart
@@ -1,32 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_drive/application/book_loader/book_loader_cubit.dart';
 import 'package:test_drive/application/book_view/book_view_cubit.dart';
-import 'package:test_drive/domain/book/book_repository.dart';
 import 'package:test_drive/presentation/pages/library_page/components/book_grid/book_grid.dart';
 import 'package:test_drive/presentation/pages/library_page/components/book_list/book_list.dart';
-
-import '../../../../domain/book/book.dart';
 
 class BookView extends StatelessWidget {
   const BookView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    BookRepository bookRepository = BookRepository(Theme.of(context).platform);
-    return FutureBuilder(
-        future: bookRepository.getBooks(),
-        builder: ((context, snapshot) =>
-            BlocBuilder<BookViewCubit, BookViewState>(
-              builder: (context, state) {
-                if (snapshot.hasData) {
-                  if (state.isGridView) {
-                    return BookGrid(books: snapshot.data as List<Book>);
-                  } else {
-                    return BookList(books: snapshot.data as List<Book>);
-                  }
-                }
-                return const CircularProgressIndicator();
-              },
-            )));
+    return BlocBuilder<BookLoaderCubit, BookLoaderState>(
+      builder: ((context, loaderState) {
+        return loaderState.map(
+          initial: (_) => const CircularProgressIndicator.adaptive(),
+          loading: (_) => const CircularProgressIndicator.adaptive(),
+          loadSuccess: (e) => BlocBuilder<BookViewCubit, BookViewState>(
+            builder: (context, viewState) {
+              if (viewState.isGridView) {
+                return BookGrid(books: e.books);
+              } else {
+                return BookList(books: e.books);
+              }
+            },
+          ),
+          loadFailure: (e) => Center(
+            child: Text('Could not load books because: ${e.errorMsg}'),
+          ),
+        );
+      }),
+    );
   }
 }
--- lib/presentation/wrapper.dart
+++ lib/presentation/wrapper.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_drive/application/book_loader/book_loader_cubit.dart';
 import 'package:test_drive/application/book_view/book_view_cubit.dart';
 import 'package:test_drive/presentation/homepage.dart';
 
 import '../application/page_selector/page_selector_cubit.dart';
+import '../domain/book/book_repository.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    BookRepository bookRepository = BookRepository(Theme.of(context).platform);
+
     return MultiBlocProvider(
       providers: [
         BlocProvider(
@@ -17,6 +21,9 @@ class Wrapper extends StatelessWidget {
         ),
         BlocProvider(
           create: (context) => BookViewCubit(),
+        ),
+        BlocProvider(
+          create: (context) => BookLoaderCubit(bookRepository)..getBooks(),
         ),
       ],
       child: const MyHomePage(),
